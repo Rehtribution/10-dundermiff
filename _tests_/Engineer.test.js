64678e489699891784cf335fce8b6d2f05629037
Reht Cavazos <reht.dev@gmail.com>
2022-01-07 19:58:55 -0600

    comments in the tests folder

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,22 +1,23 @@
 const Engineer = require("../lib/Engineer");
-//set test
+
+//set github username
 test("Set github username with constructor function", () => {
     const gitUser = "BondJames";
     const employee = new Engineer("James Bond", "007", "email@email.com", gitUser);
     expect(employee.github).toBe(gitUser);
 });
-//get tests:
+//get github username
 test("Get github username with constructor function", () => {
     const gitUser = "BondJames";
     const employee = new Engineer("James Bond", "007", "email@email.com", gitUser);
     expect(employee.github).toBe(gitUser);
 });
 
+//get role
 test("getRole() returns Engineer", () => {
     const role = "Engineer";
     const employee = new Engineer();
     expect(employee.getRole()).toBe(role);
 });
-//get tests end
 
 //all tests passing
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,24 +1,24 @@
 const Intern = require("../lib/Intern");
 
-//set test
+//set school name
 test("set school name with constructor function", () => {
     const testVal = "Secret Agent University";
     const employee = new Intern("James Bond", "007", "email@email.com", testVal);
     expect(employee.school).toBe(testVal);
 });
 
-//get tests:
+//get school
 test("set school name with constructor function", () => {
     const testVal = "Secret Agent University";
     const employee = new Intern("James Bond", "007", "email@email.com", testVal);
     expect(employee.getSchool()).toBe(testVal);
 });
 
+//get role
 test("getRole() returns Intern", () => {
     const role = "Intern";
     const employee = new Intern("James Bond", "007", "email@email.com","Wasteland");
     expect(employee.getRole()).toBe(role);
 });
-//get tests end
 
 //all tests passing
