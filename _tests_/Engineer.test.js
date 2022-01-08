9a7149abffb027dded59fd8ec907e0e81ba1eb94
Reht Cavazos <reht.dev@gmail.com>
2022-01-07 19:50:58 -0600

    engineer tests passing

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,12 +1,22 @@
 const Engineer = require("../lib/Engineer");
-
-test("ability to set github username with constructor function", () => {
-    const github = "Mad Max";
-    const employee = new Engineer(github);
-    expect(employee.github).toBe(github);
+//set test
+test("Set github username with constructor function", () => {
+    const gitUser = "BondJames";
+    const employee = new Engineer("James Bond", "007", "email@email.com", gitUser);
+    expect(employee.github).toBe(gitUser);
 });
+//get tests:
+test("Get github username with constructor function", () => {
+    const gitUser = "BondJames";
+    const employee = new Engineer("James Bond", "007", "email@email.com", gitUser);
+    expect(employee.github).toBe(gitUser);
+});
+
 test("getRole() returns Engineer", () => {
     const role = "Engineer";
-    const employee = new Engineer(madmax);
+    const employee = new Engineer();
     expect(employee.getRole()).toBe(role);
 });
+//get tests end
+
+//all tests passing
