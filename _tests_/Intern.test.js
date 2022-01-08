a65ac85eeb292c8ce8cdabebff6e9fbc8d839644
Reht Cavazos <reht.dev@gmail.com>
2022-01-07 19:56:39 -0600

    intern tests passing, all _tests_ passing

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,13 +1,24 @@
 const Intern = require("../lib/Intern");
 
+//set test
 test("set school name with constructor function", () => {
-    const school = "Wasteland";
-    const employee = new Intern(school);
-    expect(employee.school).toBe(school);
+    const testVal = "Secret Agent University";
+    const employee = new Intern("James Bond", "007", "email@email.com", testVal);
+    expect(employee.school).toBe(testVal);
+});
+
+//get tests:
+test("set school name with constructor function", () => {
+    const testVal = "Secret Agent University";
+    const employee = new Intern("James Bond", "007", "email@email.com", testVal);
+    expect(employee.getSchool()).toBe(testVal);
 });
 
 test("getRole() returns Intern", () => {
     const role = "Intern";
-    const employee = new Intern(Wasteland);
+    const employee = new Intern("James Bond", "007", "email@email.com","Wasteland");
     expect(employee.getRole()).toBe(role);
 });
+//get tests end
+
+//all tests passing
