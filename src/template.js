92c75dd34d902a3de7267718ab8fe759be0fffad
Reht Cavazos <reht.dev@gmail.com>
2022-01-08 18:12:05 -0600

    css editing, layout editing. cards not displaying in wrapping row correctly, commas showing between cards.

--- src/template.js
+++ src/template.js
@@ -3,9 +3,9 @@ const createTeam = (team) => {
     //framework for manager
     const createManager = (manager) => {
         return `
-            <div class="manager-container">
-                <h1>Name: ${manager.getName()}</h1>
-                <p>Email: ${manager.getEmail()}</p>
+            <div class="card">
+                <h1>${manager.getName()}</h1>
+                <p>${manager.getEmail()}</p>
                 <p>ID: ${manager.getId()}</p>
                 <p>Office Number: ${manager.getOfficeNumber()}</p>
             </div>
@@ -14,9 +14,9 @@ const createTeam = (team) => {
     //framework for engineer
     const createEngineer = (engineer) => {
         return `
-            <div class="engineer-container">
-                <h1>Name: ${engineer.getName()}</h1>
-                <p>Email: ${engineer.getEmail()}</p>
+            <div class="card">
+                <h1>${engineer.getName()}</h1>
+                <p>${engineer.getEmail()}</p>
                 <p>ID: ${engineer.getId()}</p>
                 <p><a href="https://api.github.com/users/${engineer.github}">Github: ${engineer.    getGithub()}</a></p>
             </div>
@@ -26,9 +26,9 @@ const createTeam = (team) => {
     //framework for intern
     const createIntern = (intern) => {
         return `   
-            <div class="intern-container">
-                <h1>Name: ${intern.getName()}</h1>
-                <p>Email: ${intern.getEmail()}</p>
+            <div class="card">
+                <h1>${intern.getName()}</h1>
+                <p>${intern.getEmail()}</p>
                 <p>ID: ${intern.getId()}</p>
                 <p>School: ${intern.getSchool()}</p>
             </div>
@@ -53,12 +53,25 @@ module.exports = (team) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-        <title>Team</title>
+        <link rel="stylesheet" href="../src/style.css">
+        <title>TPG:Team Profile Generator</title>
     </head>
+    
+    <header>
+        <h1>The Office Team</h1>
+    </header>
+    
+    <!--main content start-->
     <body>
         <div>${createTeam(team)}</div>
     </body>
+    <!--main content end-->
+    
+    <!--footer-->
+    <footer>
+        <h4>Team Generator Challenge 10</h4><a href="README.md">ReadMe</a>
+        &copy; 2021 Rehtribution
+    </footer>
     </html>
     `
 }
